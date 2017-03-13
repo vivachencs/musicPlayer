46ffc45b80d69504a28f26f752bf3c367c722323
ChenX <vivachencs@gmail.com>
2017-03-13 21:46:33 +0800

    添加 Ajax 函数

--- js/chen.js
+++ js/chen.js
@@ -59,3 +59,20 @@ var initCustomProps = function(element, props, type) {
         element.props = null
     }
 }
+
+// ajax
+var ajax = function(method, path, data, reseponseCallback) {
+    var r = new XMLHttpRequest()
+    // 设置请求方法和请求地址
+    r.open(method, path, true)
+    // 设置发送的数据的格式
+    r.setRequestHeader('Content-Type', 'application/json')
+    // 注册响应函数
+    r.onreadystatechange = function() {
+        if(r.readyState === 4) {
+            reseponseCallback(r)
+        }
+    }
+    // 发送请求
+    r.send(data)
+}
